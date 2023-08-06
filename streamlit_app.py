d72430ecaf6988a4c6ac4454783d2088475720fc
conroyp02gmailcom <141515805+conroyp02gmailcom@users.noreply.github.com>
2023-08-06 13:50:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choice")
 ## COMMENTED OUT TO REMOVE JSON LINE streamlit.text(fruityvice_response.json())
 
 # Normalises json
